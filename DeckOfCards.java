16561ebf492b4fef4d9c16d45d9bc1c206963be1
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-09-09 08:40:10 +0530

    ADDED: Welcome message

---
+++ DeckOfCards.java
@@ -0,0 +1,10 @@
+package com.workshop;
+
+/**
+ * @purpose: Welcome message.
+ */
+public class DeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Deck Of Cards");
+    }
+}
