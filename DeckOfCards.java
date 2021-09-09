dbebeb33c5a208cb7cee925c9d11d823e6caea3f
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-09-09 09:16:48 +0530

    ADDED: Initial Game with Deck Of Cards

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,10 +1,20 @@
 package com.workshop;
 
 /**
- * @purpose: Welcome message.
+ * @purpose: Initialize deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades")
+ * & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace").
  */
 public class DeckOfCards {
+    private Card theCard;
+    private int remainingCards = 52;
+
+    DeckOfCards() {
+        theCard = new Card();
+    }
+
+    //main method
     public static void main(String[] args) {
-        System.out.println("Welcome to Deck Of Cards");
+        System.out.println("Welcome to deck of cards");
+
     }
 }
